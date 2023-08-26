4f725e72aae8bb7d898fa7551c718c97a11fc624
jessica_Silvaj <jessicasilva.jesusOfc@gmail.com>
2023-08-26 19:31:55 -0300

    Adicionan o template de loja e fazendo algumas configurações

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
 });
 
+Route::get('/', [HomeController::class,'index'])->name('home.index');
 Route::get('/redirect', [HomeController::class,'redirect']);
